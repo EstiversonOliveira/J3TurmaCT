5c3aadb11d95c848e6d6ecea5d8b39fc5bd74c5f
Estíverson Oliveira <107802352+EstiversonOliveira@users.noreply.github.com>
2024-11-07 15:06:04 -0300

    updated final R6

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -9,12 +9,14 @@ const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
 const horas = parseInt(dados.tempo_medio)
 const minutos = Math.round((dados.tempo_medio - horas) * 60)
 
+const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
+
 
     console.log(dados);
 
     const paragrafo = document.createElement("p");
     paragrafo.classList.add("graficos-container__texto");
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas?`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> bilhões de pessoas e que aproximadamente <span>${pessoasConectadas}</span> bilhões estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> pessoas conectadas em alguma rede social.`
     const container = document.getElementById("graficos-container");
     container.appendChild(paragrafo);
 }
